d1a53d760ede337c0a169db1836654b56f0c6631
RexShack <rexshackuk@gmail.com>
2022-12-29 21:55:23 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,9 +5,29 @@ Config.DistanceSpawn = 20.0 -- Distance before spawning/despawning the ped. (GTA
 
 Config.PedList = {
 
-    {
-        model = `A_M_M_ASBMINER_02`,
-        coords = vector4(2332.7944, -1479.724, 45.946334, 151.19625),
+    {    -- gunsmith Valentine 
+        model = `U_M_M_ValGunsmith_01`,
+        coords = vector4(-281.25, 778.88, 119.5, 0.0),
+    },
+    {    -- doctor Valentine 
+        model = `U_M_M_ValDoctor_01`,
+        coords = vector4(-288.04, 804.16, 119.39, 270.0),
+    },
+    {    -- trainstation worker Valentine 
+        model = `S_M_M_TrainStationWorker_01`,
+        coords = vector4(-175, 631.88, 114.09, 323.0),
+    },
+    {    -- trainstation worker Valentine 
+        model = `S_M_M_TrainStationWorker_01`,
+        coords = vector4(-177.94, 628.09, 114.09, 154.0),
+    },
+    {    -- general store Valentine 
+        model = `U_M_M_ValGenStoreOwner_01`,
+        coords = vector4(-324.14, 803.54, 117.88, 277.0),
+    },
+    {    -- trainstation worker Valentine 
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-339.26, 767.7, 116.57, 103.16),
     },
 
 }
