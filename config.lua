52e344741c65fe038e17b604e1223fb4d8e83f5b
RexShack <rexshackuk@gmail.com>
2022-12-30 05:44:34 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -25,17 +25,14 @@ Config.PedList = {
         model = `U_M_M_ValGenStoreOwner_01`,
         coords = vector4(-324.14, 803.54, 117.88, 277.0),
     },
-    {    -- trainstation worker Valentine 
-        model = `U_M_M_VALBUTCHER_01`,
-        coords = vector4(-339.26, 767.7, 116.57, 103.16),
-    },
-    {    -- trapper vendor Valentine 
-        model = `U_M_M_SDTRAPPER_01`,
-        coords = vector4(-333.9737, 773.49157, 116.22194, 111.8759),
-    },
     {    -- fish vendor Valentine 
         model = `CS_FISHCOLLECTOR`,
         coords = vector4(-335.4444, 762.00537, 116.5845, 45.516292),
+    },
+	-- trappers
+    {    -- trapper vendor Valentine 
+        model = `U_M_M_SDTRAPPER_01`,
+        coords = vector4(-333.9737, 773.49157, 116.22194, 111.8759),
     },
     {    -- trapper vendor Saint Denis 
         model = `U_M_M_SDTRAPPER_01`,
@@ -56,6 +53,47 @@ Config.PedList = {
     {    -- trapper vendor Tumbleweed 
         model = `U_M_M_SDTRAPPER_01`,
         coords = vector4(-5511.721, -2951.048, -1.83548, 165.87483),
+    },
+	-- butchers
+    {    -- butcher Valentine 
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-339.26, 767.7, 116.57, 103.16),
+    },
+    {    -- butcher Saint Denis
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(2817.6848, -1323.25, 46.607814, 54.587085),
+    },
+    {    -- butcher Rhodes
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(1297.3735, -1277.661, 75.876304, 158.4201),
+    },
+    {    -- butcher Annesburg
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(2934.1706, 1301.2891, 44.483638, 78.346809),
+    },
+    {    -- butcher Tumbleweed
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-5509.831, -2947.271, -1.89185, 256.48596),
+    },
+    {    -- butcher Blackwater
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-753.0086, -1284.84, 43.470008, 267.18395),
+    },
+    {    -- butcher Strawberry
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-1753.137, -392.8364, 156.24348, 189.32403),
+    },
+    {    -- butcher Van Horn
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(2992.4711, 572.20001, 44.365322, 263.94104),
+    },
+    {    -- butcher Spider Gorge
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-1356.811, 2420.0056, 307.49148, 301.39794),
+    },
+    {    -- butcher Riggs Station
+        model = `U_M_M_VALBUTCHER_01`,
+        coords = vector4(-1007.92, -541.2982, 99.108978, 281.41009),
     },
 
 }
