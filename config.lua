89a164cdf7e8ef890f6b99a7b4ebe220adb02083
RexshackGaming <rexshackuk@gmail.com>
2023-03-05 20:15:42 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -187,4 +187,28 @@ Config.PedList = {
         model = `A_M_O_SDUpperClass_01`,
         coords = vector4(2596.5463, -1299.845, 52.817153, 304.04638),
     },
+    {   -- delivery Saint Denis
+        model = `A_M_M_SDDockForeman_01`,
+        coords = vector4(2904.1989, -1169.292, 46.112228, 96.722068),
+    },
+    {   -- delivery Valentine 
+        model = `A_M_M_FOREMAN`,
+        coords = vector4(-339.0577, 814.22424, 116.96039, 125.19566),
+    },
+    {   -- delivery Blackwater 
+        model = `A_M_M_FOREMAN`,
+        coords = vector4(-743.7046, -1218.822, 43.29129, 94.302909),
+    },
+    {   -- delivery Strawberry
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-1798.899, -425.6275, 156.37739, 352.46316),
+    },
+    {   -- delivery Mcfarlands Ranch
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-2357.585, -2367.583, 62.18066, 168.52516),
+    },
+    {   -- delivery Tumbleweed
+        model = `A_M_M_SDDockForeman_01`,
+        coords = vector4(-5529.143, -2932.52, -1.95342, 212.60365),
+    },
 }
