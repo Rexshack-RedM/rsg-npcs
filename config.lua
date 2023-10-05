32aab58164237330e7ebb50e2b18956908585aff
RexshackGaming <rexshackuk@gmail.com>
2023-10-05 17:07:05 +0100

    beekeeper support

--- config.lua
+++ config.lua
@@ -199,5 +199,30 @@ Config.PedList = {
         model = `u_m_o_rigtrainstationworker_01`,
         coords = vector4(1268.4835, -6850.459, 43.318496, 212.38269),
     },
+    --beekeepers
+    {   -- valentine beekeeper shop
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-468.2546, 862.86431, 126.88754, 169.2465),
+    },
+    {   -- strawberry beekeeper shop 
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-1642.163, -339.113, 172.64041, 145.73445),
+    },
+    {   -- blackwater beekeeper shop
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-876.5037, -1087.799, 58.660987, 122.2913),
+    },
+    {   -- mcfarlanes beekeeper shop
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(-2301.1, -2382.684, 63.184398, 85.783271),
+    },
+    {   -- rhodes beekeeper
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(1396.6099, -1115.501, 75.171989, 116.12648),
+    },
+    {   -- braitewaite beekeeper
+        model = `U_M_M_BiVForeman_01`,
+        coords = vector4(855.49353, -1892.614, 44.397026, 219.83865),
+    },
 
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'rdr3'
 
 author 'RexShack#3041'
 description 'rsg-npcs'
-version '1.0.0'
+version '1.0.1'
 
 shared_scripts {
     'config.lua'
