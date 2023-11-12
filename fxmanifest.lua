c924529bf1e5641ee5943d434a5d87d68f7924c3
RexshackGaming <rexshackuk@gmail.com>
2023-11-12 09:02:33 +0000

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-author 'RexShack#3041'
 description 'rsg-npcs'
-version '1.0.2'
+version '1.0.3'
 
 shared_scripts {
     'config.lua'
