30ac9fb22e42be199a22f0b85b88102388c9649d
RexshackGaming <rexshackuk@gmail.com>
2024-04-27 16:44:30 +0100

    ox_lib and natives

--- client/client.lua
+++ client/client.lua
@@ -2,9 +2,9 @@ local spawnedPeds = {}
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(500)
+        Wait(500)
         for k,v in pairs(Config.PedList) do
-            local playerCoords = GetEntityCoords(PlayerPedId())
+            local playerCoords = GetEntityCoords(cache.ped)
             local distance = #(playerCoords - v.coords.xyz)
 
             if distance < Config.DistanceSpawn and not spawnedPeds[k] then
@@ -15,7 +15,7 @@ Citizen.CreateThread(function()
             if distance >= Config.DistanceSpawn and spawnedPeds[k] then
                 if Config.FadeIn then
                     for i = 255, 0, -51 do
-                        Citizen.Wait(50)
+                        Wait(50)
                         SetEntityAlpha(spawnedPeds[k].spawnedPed, i, false)
                     end
                 end
@@ -29,26 +29,26 @@ end)
 function NearPed(model, coords)
     RequestModel(model)
     while not HasModelLoaded(model) do
-        Citizen.Wait(50)
+        Wait(50)
     end
     spawnedPed = CreatePed(model, coords.x, coords.y, coords.z - 1.0, coords.w, false, false, 0, 0)
     SetEntityAlpha(spawnedPed, 0, false)
-    Citizen.InvokeNative(0x283978A15512B2FE, spawnedPed, true)
+    SetRandomOutfitVariation(spawnedPed, true)
     SetEntityCanBeDamaged(spawnedPed, false)
     SetEntityInvincible(spawnedPed, true)
     FreezeEntityPosition(spawnedPed, true)
     SetBlockingOfNonTemporaryEvents(spawnedPed, true)
     -- set relationship group between npc and player
-    Citizen.InvokeNative(0xC80A74AC829DDD92, spawnedPed, GetPedRelationshipGroupHash(spawnedPed)) -- SetPedRelationshipGroupHash
-    Citizen.InvokeNative(0xBF25EB89375A37AD, 1, GetPedRelationshipGroupHash(spawnedPed), `PLAYER`) -- SetRelationshipBetweenGroups
+    SetPedRelationshipGroupHash(spawnedPed, GetPedRelationshipGroupHash(spawnedPed))
+    SetRelationshipBetweenGroups(1, GetPedRelationshipGroupHash(spawnedPed), `PLAYER`)
     if Config.Debug then
-        local relationship = Citizen.InvokeNative(0x9E6B70061662AE5C, GetPedRelationshipGroupHash(spawnedPed), `PLAYER`) -- GetRelationshipBetweenGroups
+        local relationship = GetRelationshipBetweenGroups(GetPedRelationshipGroupHash(spawnedPed), `PLAYER`)
         print(relationship)
     end
     -- end of relationship group
     if Config.FadeIn then
         for i = 0, 255, 51 do
-            Citizen.Wait(50)
+            Wait(50)
             SetEntityAlpha(spawnedPed, i, false)
         end
     end
