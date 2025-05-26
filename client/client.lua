a19ebaa0fd441c7923f638754ba75d1c89a79354
Ramazan  TOY <ramcotoy06@gmail.com>
2025-05-26 22:06:17 +0300

    timeout added on npc spawn,  version changed

--- client/client.lua
+++ client/client.lua
@@ -3,7 +3,7 @@ local spawnedPeds = {}
 CreateThread(function()
     while true do
         Wait(500)
-        for k,v in pairs(Config.PedList) do
+        for k, v in pairs(Config.PedList) do
             local playerCoords = GetEntityCoords(PlayerPedId())
             local distance = #(playerCoords - v.coords.xyz)
 
@@ -11,7 +11,7 @@ CreateThread(function()
                 local spawnedPed = NearPed(v.model, v.coords)
                 spawnedPeds[k] = { spawnedPed = spawnedPed }
             end
-            
+
             if distance >= Config.DistanceSpawn and spawnedPeds[k] then
                 if Config.FadeIn then
                     for i = 255, 0, -51 do
@@ -28,9 +28,16 @@ end)
 
 function NearPed(model, coords)
     RequestModel(model)
-    while not HasModelLoaded(model) do
+
+    local timeout = 0
+    while not HasModelLoaded(model) and timeout < 100 do
         Wait(50)
+        timeout = timeout + 1
     end
+
+    
+    if not HasModelLoaded(model) then return end
+
     spawnedPed = CreatePed(model, coords.x, coords.y, coords.z - 1.0, coords.w, false, false, 0, 0)
     SetEntityAlpha(spawnedPed, 0, false)
     SetRandomOutfitVariation(spawnedPed, true)
@@ -51,7 +58,7 @@ end
 -- cleanup
 AddEventHandler("onResourceStop", function(resourceName)
     if GetCurrentResourceName() ~= resourceName then return end
-    for k,v in pairs(spawnedPeds) do
+    for k, v in pairs(spawnedPeds) do
         DeletePed(spawnedPeds[k].spawnedPed)
         spawnedPeds[k] = nil
     end
